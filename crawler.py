3c92de3fd521e8eaa3c3a70ab8370188b8d7b278
dlacroix1 <dlacroix@aol.com>
2014-11-02 21:39:16 -0600

    Working Crawler
    Worked with entire team for this commit and previous.

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ import json
 import Queue
 import time
 import random
+from copy import deepcopy
 
 key = 'F6B38B6772012F11E1BB68FCF696140A'
 getOwnedGames = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='
@@ -16,25 +17,33 @@ user_owned_games = {}
 def get_games(steamid):
     queue.put(steamid)
     counter = 0
-    while not queue.empty() and counter < 50:
+    while not queue.empty() and counter < 1:
+        dictionary_file = open('dictionary.txt', 'a')
+        queue_file = open('queue.txt', 'w')
         current_user = str(queue.get())
-        print current_user
+        print current_user, counter
         counter += 1
         summaries = json.loads(urllib2.urlopen(getSummaries + key + '&steamids=' + current_user + '&format=json').read())
         if summaries['response']['players'][0]['communityvisibilitystate'] == 3:
-            owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&format=json').read())
+            owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&include_played_free_games=1&format=json').read())
             user_owned_games[current_user] = owned_games
+            dictionary_file.write(str(current_user) + str(owned_games) + '\n')
+            dictionary_file.close()
             friend_list = json.loads(urllib2.urlopen(getFriendsList + key + '&steamid=' + current_user + '&format=json').read())
             for friend in friend_list['friendslist']['friends']:
                 friend_id = friend['steamid']
                 if friend_id not in user_set:
                     queue.put(friend_id)
                     user_set.append(friend_id)
+        #while not temp_queue.empty():
+         #   queue_file.write(temp_queue.get() + '\n')
+        for elem in list(queue.queue):
+            queue_file.write(str(elem) + '\n')
+        queue_file.close()
         time.sleep(random.randint(3, 5))
 
 
 def main():
     get_games('76561198053212280')
-    print user_owned_games.keys()
 
 main()
