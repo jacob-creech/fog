f58802b957c3359f6f470aad9d65acbb0420dca0
sbierwagen <sbierwagen@neo.tamu.edu>
2014-11-03 23:33:34 -0600

    Made the crawler more resilient to URL error. If an error occurs at any of the three requests, then the steam id is pushed back onto the queue and the program continues on.

--- crawler.py
+++ crawler.py
@@ -18,19 +18,41 @@ user_owned_games = {}
 def get_games(steamid):
     queue.put(steamid)
     counter = 0
-    while not queue.empty() and counter < 3:
+    while not queue.empty():
         dictionary_file = open('dictionary.txt', 'a')
-        queue_file = open('queue.txt', 'w')
+        
         current_user = str(queue.get())
         print current_user, counter
         counter += 1
-        summaries = json.loads(urllib2.urlopen(getSummaries + key + '&steamids=' + current_user + '&format=json').read())
+        try:
+            summaries = json.loads(urllib2.urlopen(getSummaries + key + '&steamids=' + current_user + '&format=json', timeout = 10).read())
+        except urllib2.URLError, e:
+            print "URLError: ", e.reason
+            count -= 1
+            queue.put(current_user)
+            continue
+        
         if summaries['response']['players'][0]['communityvisibilitystate'] == 3:
-            owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&include_played_free_games=1&format=json').read())
+            try:
+                owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&include_played_free_games=1&format=json', timeout = 10).read())
+            except urllib2.URLError, e:
+                print "URLError: ", e.reason
+                count -= 1
+                queue.put(current_user)
+                continue
+            
             user_owned_games[current_user] = owned_games
             dictionary_file.write(str(current_user) + str(owned_games) + '\n')
             dictionary_file.close()
-            friend_list = json.loads(urllib2.urlopen(getFriendsList + key + '&steamid=' + current_user + '&format=json').read())
+
+            try:
+                friend_list = json.loads(urllib2.urlopen(getFriendsList + key + '&steamid=' + current_user + '&format=json', timeout = 10).read())
+            except urllib2.URLError, e:
+                print "URLError: ", e.reason
+                count -= 1
+                queue.put(current_user)
+                continue
+            
             for friend in friend_list['friendslist']['friends']:
                 friend_id = friend['steamid']
                 if friend_id not in user_set:
@@ -38,10 +60,11 @@ def get_games(steamid):
                     user_set.append(friend_id)
         #while not temp_queue.empty():
          #   queue_file.write(temp_queue.get() + '\n')
+        queue_file = open('queue.txt', 'w')
         for elem in list(queue.queue):
             queue_file.write(str(elem) + '\n')
         queue_file.close()
-        time.sleep(random.randint(3, 5))
+        time.sleep(random.randint(1, 2))
 
 
 def main():
