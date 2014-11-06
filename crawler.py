57860d765c9d8f9feaa97d2274e681cd45ac1acf
dlacroix1 <dlacroix@aol.com>
2014-11-06 09:34:26 -0600

    Fix Index out-of-Bounds Error
    The only change was adding line 45 to verify that line 46 would not get
    an index out of bounds error. Everything else just needed to be tabbed
    over.

--- crawler.py
+++ crawler.py
@@ -42,59 +42,59 @@ def get_games(steamid):
             print "User ID:", current_user
             queue.put(current_user)
             continue
-        
-        if summaries['response']['players'][0]['communityvisibilitystate'] == 3:
-            try:
-                owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&include_played_free_games=1&format=json', timeout = 10).read())
-            except urllib2.URLError, e:
-                print "URLError: ", e.reason
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
-            except urllib2.HTTPError, e:
-                print "HTTPError: ", e.code
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
-            except:
-                e = sys.exc_info()[0]
-                print e
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
+        if 'response' in summaries and 'players' in summaries['response'] and len(summaries['response']['players']) > 0 and 'communityvisibilitystate' in summaries['response']['players'][0]:
+            if summaries['response']['players'][0]['communityvisibilitystate'] == 3:
+                try:
+                    owned_games = json.loads(urllib2.urlopen(getOwnedGames + key + '&steamid=' + current_user + '&include_played_free_games=1&format=json', timeout = 10).read())
+                except urllib2.URLError, e:
+                    print "URLError: ", e.reason
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
+                except urllib2.HTTPError, e:
+                    print "HTTPError: ", e.code
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
+                except:
+                    e = sys.exc_info()[0]
+                    print e
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
 
-            user_owned_games[current_user] = owned_games
-            dictionary_file.write(str(current_user) + ' ' + str(owned_games) + '\n')
-            dictionary_file.close()
-            counter += 1
-            print current_user, counter
+                user_owned_games[current_user] = owned_games
+                dictionary_file.write(str(current_user) + ' ' + str(owned_games) + '\n')
+                dictionary_file.close()
+                counter += 1
+                print current_user, counter
 
-            try:
-                friend_list = json.loads(urllib2.urlopen(getFriendsList + key + '&steamid=' + current_user + '&format=json', timeout = 10).read())
-            except urllib2.URLError, e:
-                print "URLError: ", e.reason
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
+                try:
+                    friend_list = json.loads(urllib2.urlopen(getFriendsList + key + '&steamid=' + current_user + '&format=json', timeout = 10).read())
+                except urllib2.URLError, e:
+                    print "URLError: ", e.reason
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
 
-            except urllib2.HTTPError, e:
-                print "HTTPError: ", e.code
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
+                except urllib2.HTTPError, e:
+                    print "HTTPError: ", e.code
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
 
-            except:
-                e = sys.exc_info()[0]
-                print e
-                print "User ID:", current_user
-                queue.put(current_user)
-                continue
-            
-            for friend in friend_list['friendslist']['friends']:
-                friend_id = friend['steamid']
-                if friend_id not in user_set:
-                    queue.put(friend_id)
-                    user_set.append(friend_id)
+                except:
+                    e = sys.exc_info()[0]
+                    print e
+                    print "User ID:", current_user
+                    queue.put(current_user)
+                    continue
+
+                for friend in friend_list['friendslist']['friends']:
+                    friend_id = friend['steamid']
+                    if friend_id not in user_set:
+                        queue.put(friend_id)
+                        user_set.append(friend_id)
         #while not temp_queue.empty():
          #   queue_file.write(temp_queue.get() + '\n')
         queue_file = open('queue.txt', 'w')
